af482e18c67e23f8740078fa0b494624f73839a9
Darius Otelea <darius.otelea@luminos.software>
2019-02-26 10:12:08 +0200

    Seeds WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 users = User.create([
-  {name: 'John'},
-  {name: 'Michael'},
-  {name: 'Robert'},
-  {name: 'Carmen'},
-  {name: 'Andreea'},
-  {name: 'Silvana'},
-  {name: 'Maria'},
-  {name: 'Andra'},
-  {name: 'Ioana'},
-  {name: 'Darius'},
+  { name: 'John' },
+  { name: 'Michael' },
+  { name: 'Robert' },
+  { name: 'Carmen' },
+  { name: 'Andreea' },
+  { name: 'Silvana' },
+  { name: 'Maria' },
+  { name: 'Andra' },
+  { name: 'Ioana' },
+  { name: 'Darius' },
+])
+
+stories = Story.create([
+  { description: 'This description should always be more than 20 words or smth ---- 1', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 2', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 3', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 4', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 5', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 6', picture: 'Imagine nice picture here' },
+  { description: 'This description should always be more than 20 words or smth ---- 7', picture: 'Imagine nice picture here' }
+])
+
+user_stories = UserStory.create([
+  { user_id: 1, story_id: stories.first },
+  { user_id: 2, story_id: 4 },
+  { user_id: 3, story_id: 2 },
+  { user_id: 4, story_id: 3 }
 ])
