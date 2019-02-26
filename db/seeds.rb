37759aeb465857801cb07a95f70729cf8694a270
Muresan Beniamin <ben.muresan@luminos.software>
2019-02-26 10:23:14 +0200

    wip seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([
-  { name: 'John' },
-  { name: 'Michael' },
-  { name: 'Robert' },
-  { name: 'Carmen' },
-  { name: 'Andreea' },
-  { name: 'Silvana' },
-  { name: 'Maria' },
-  { name: 'Andra' },
-  { name: 'Ioana' },
-  { name: 'Darius' },
-])
+# users = User.create([
+#   { name: 'John' },
+#   { name: 'Michael' },
+#   { name: 'Robert' },
+#   { name: 'Carmen' },
+#   { name: 'Andreea' },
+#   { name: 'Silvana' },
+#   { name: 'Maria' },
+#   { name: 'Andra' },
+#   { name: 'Ioana' },
+#   { name: 'Darius' },
+# ])
+#
+# stories = Story.create([
+#   { description: 'This description should always be more than 20 words or smth ---- 1', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 2', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 3', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 4', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 5', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 6', picture: 'Imagine nice picture here' },
+#   { description: 'This description should always be more than 20 words or smth ---- 7', picture: 'Imagine nice picture here' }
+# ])
+#
+# user_stories = UserStory.create([
+#   { user_id: 1, story_id: stories.first },
+#   { user_id: 2, story_id: 4 },
+#   { user_id: 3, story_id: 2 },
+#   { user_id: 4, story_id: 3 }
+# ])
 
-stories = Story.create([
-  { description: 'This description should always be more than 20 words or smth ---- 1', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 2', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 3', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 4', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 5', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 6', picture: 'Imagine nice picture here' },
-  { description: 'This description should always be more than 20 words or smth ---- 7', picture: 'Imagine nice picture here' }
-])
+user1 = User.where(name: 'Ben').first_or_create
+story1 = Story.where(description: "Deep blue sea", picture: "image.png").first_or_create
+user2 = User.where(name: 'Darius').first_or_create
+story2 = Story.where(description: "Deep black sea", picture: "image1.png").first_or_create
 
-user_stories = UserStory.create([
-  { user_id: 1, story_id: stories.first },
-  { user_id: 2, story_id: 4 },
-  { user_id: 3, story_id: 2 },
-  { user_id: 4, story_id: 3 }
-])
+UserStory.where(user: user1, story: story1).first_or_create
+UserStory.where(user: user2, story: story2).first_or_create
