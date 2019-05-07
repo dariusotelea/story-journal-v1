783e8c5220dbd81925849b05d9c28effabf2b30b
Darius Otelea <darius.otelea@luminos.software>
2019-05-07 11:15:57 +0300

    Restructured app format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user_stories
-  resources :stories
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :user_stories
+      resources :stories
+      resources :users
+    end
+  end
 end
